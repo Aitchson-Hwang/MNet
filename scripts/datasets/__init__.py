083d536c1ebc2be38e0c032edd65c58254988498
Aitchson-Hwang <131634331+Aitchson-Hwang@users.noreply.github.com>
2025-01-11 16:54:56 +0800

    Add files via upload

---
+++ scripts/datasets/__init__.py
@@ -0,0 +1,11 @@
+from .COCO import COCO
+from .BIH import BIH
+from .COCO_without_wm import COCO_without_wm
+from .COCO_real import COCO_real
+from .COCO_real_png import COCO_real_png
+from .CLWD import CLWD
+# from .CLWD import CLWDtrain
+from .COCOF import COCOF
+from .LVW import LVW
+from .WVData import wvdata
+__all__ = ('COCO','BIH')
